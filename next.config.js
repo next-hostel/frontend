521bf2092c1bc980af79b0575e95041ed9c1f22c
HARSH TRIPATHI <tripathihk2014@gmail.com>
2021-10-05 08:16:51 +0530

    change in next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     publicRuntimeConfig: {
         APP_NAME: 'ZepHep',
         API_DEVELOPMENT: 'http://localhost:8000',
-        API_PRODUCTION: 'https://multiuser-blogging.herokuapp.com/api',
+        API_PRODUCTION: 'https://ziphipapi.herokuapp.com',
         PRODUCTION: false,
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
         DOMAIN_PRODUCTION: 'https://hostelstartup.herokuapp.com'
