c66373eaca1d88d9b40bd4fa5b7900a283e0a84a
HARSH TRIPATHI <tripathihk2014@gmail.com>
2021-10-05 08:09:52 +0530

    config file changes

--- config.js
+++ config.js
@@ -3,12 +3,10 @@
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION
-    ? publicRuntimeConfig.API_PRODUCTION
-    : publicRuntimeConfig.API_DEVELOPMENT;
+export const API = publicRuntimeConfig.API_PRODUCTION;
+    
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
-export const DOMAIN = publicRuntimeConfig.PRODUCTION
-? publicRuntimeConfig.DOMAIN_PRODUCTION
-: publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+export const DOMAIN =  publicRuntimeConfig.DOMAIN_PRODUCTION;
+
