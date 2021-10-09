a10db5fa68c594a4e5ca77db61e9e79ca523b0d3
HARSH TRIPATHI <tripathihk2014@gmail.com>
2021-10-09 20:44:34 +0530

    gitignore

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    publicRuntimeConfig: {
+    env: {
         APP_NAME: 'ZepHep',
         API_DEVELOPMENT: 'http://localhost:8000',
         API_PRODUCTION: 'https://ziphipapi.herokuapp.com',
         PRODUCTION: true,
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-        DOMAIN_PRODUCTION: 'https://hostelstartup.herokuapp.com'
+        DOMAIN_PRODUCTION: 'https://hostelstartup.herokuapp.com',
+        GOOGLE_CLIENT_ID: '225888385191-bmtbpuvrkjfnlmkc2gtg03n9io69q15k.apps.googleusercontent.com',
     }
 };
