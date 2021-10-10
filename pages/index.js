aa50f8f63f08c48598908841724504a08a878695
Raman Madhukar <ramanmadhukar6@gmail.com>
2021-10-10 23:33:07 +0530

    Removing head import

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+// import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/Navbar';
 import Carousel from 'react-elastic-carousel';
