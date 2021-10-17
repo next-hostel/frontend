59e7d9f674f0b800fff012847571b57abe13a4a8
HARSH TRIPATHI <tripathihk2014@gmail.com>
2021-10-17 13:14:17 +0530

    Wromg Path in Navbar

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SignupC from '../../components/auth/Register/UserRegister/SignupC'
 
-import Navbar from "../components/Navbar"
+import Navbar from "../../components/Navbar"
 
 const Signup = () => {
     return (
